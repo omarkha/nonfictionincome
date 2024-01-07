a0a62c542296e0be467aaa96e363582a6fd02446
Copyres <copyresearcher>
2024-01-07 18:40:47 -0500

    asdasdasd

--- client/src/App.js
+++ client/src/App.js
@@ -46,7 +46,7 @@ function App(props) {
 
   const handleUserId = async (fireID) => {
 
-    await axios.get(`${uri}/api/users/firebase/${fireID}`).then(res => { console.log(res); console.log(res.data[0]._id); props.setUserID(res.data[0]._id) }).then(() => console.log(props.userState.user_id)).catch(err => console.log(err))
+    await axios.get(`${uri}/api/users/firebase/`, { fireid: fireID }).then(res => { console.log(res); console.log(res.data[0]._id); props.setUserID(res.data[0]._id) }).then(() => console.log(props.userState.user_id)).catch(err => console.log(err))
   }
 
   const initialOptions = {
