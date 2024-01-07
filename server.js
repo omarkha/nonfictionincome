e301aa8e66fe3ce1d832fa193170144b2816361d
Copyres <copyresearcher>
2024-01-06 20:48:31 -0500

    env config

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const logger = require('morgan')
 const cors = require('cors')
+require('dotenv').config()
 const PORT = process.env.PORT || 3001
 
 const app = express()
