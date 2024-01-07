7fa7cbbe25e1d3d01b569cf0a3187b38e2ea2d5a
Copyres <copyresearcher>
2024-01-06 19:06:21 -0500

    touch ups

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -8,7 +8,7 @@ import paypal from "../media/paypal.png";
 import Newsletter from '../components/NewsLetter';
 import frontbanner from "../media/homebanner4.png"
 import { useNavigate } from 'react-router-dom';
-import { Typography } from "@material-ui/core";
+
 
 const HomePage = () => {
 
@@ -27,34 +27,34 @@ const HomePage = () => {
 
             <div className='home-copy'>
 
-                <Typography variant="h3">The Non-Fiction Income Method</Typography><br /><br />
-                <Typography paragraph align="left">
+                <h3>The Non-Fiction Income Method</h3><br /><br />
+                <p>
                     Are you tired of waiting for the perfect moment to start making money online? Look no further. We believe that everyone deserves the opportunity to achieve financial freedom, regardless of their background or expertise in entrepreneurship. That's why we've created The Non-Fiction Income Method - the ultimate practical shortcut to kickstart your rewarding entrepreneurial journey right away.
-                </Typography><br /><br />
-                <Typography variant="h5" align="left">Get To Action FAST</Typography>
-                <Typography paragraph align="left">
+                </p><br /><br />
+                <h5>Get To Action FAST</h5>
+                <p>
                     Our actionable package empowers you to build your entire online business within just ONE day. Say goodbye to the frustrations of slow progress and hello to quick, tangible results. The Non-Fiction Income Method is designed to motivate you, boost your confidence, and propel you towards your dreams.
-                </Typography>
+                </p>
                 <br /><br />
-                <Typography variant="h5" align="left">Choose Your Perfect Customers</Typography>
-                <Typography paragraph align="left">
+                <h5>Choose Your Perfect Customers</h5>
+                <p>
                     One of the biggest challenges for small business owners is market research. But with The Non-Fiction Income Method, you won't have to struggle anymore. We'll share our unique psychographic approach to market research, giving you deep insights into your customer's psychology and the strategy to profit from your findings. Our eBook will equip you with a competitive advantage to conquer markets.
-                </Typography>
+                </p>
                 <br /><br />
-                <Typography variant="h5" align="left">Use Our Online Tool</Typography>
-                <Typography paragraph align="left">
+                <h5>Use Our Online Tool</h5>
+                <p>
                     But we don't stop there. We understand that an eBook alone might limit your potential. That's why we offer more than just knowledge. With The Non-Fiction Income Method, you'll gain access to our online business builder tool. This comprehensive tool provides step-by-step guidance, ready-made templates, and valuable resources to streamline your business setup process. Whether you're a beginner or an experienced entrepreneur, our tool will save you time and effort, allowing you to focus on what truly matters - growing your business.
-                </Typography>
+                </p>
                 <br /><br />
-                <Typography variant="h5" align="left">The Gift That Keeps Giving</Typography>
-                <Typography paragraph align="left">
+                <h5>The Gift That Keeps Giving</h5>
+                <p>
                     And that's not all. When you join The Non-Fiction Income Method, you become part of our premium membership community. Connect with like-minded individuals who share your entrepreneurial drive. Seek advice, collaborate, and inspire each other on your journey towards financial freedom. Together, we'll create an unstoppable force of motivated entrepreneurs, supporting and pushing each other to achieve greatness.
-                </Typography>
+                </p>
                 <br /><br />
-                <Typography variant="h5" align="left">Are You Ready For REAL Income?</Typography>
-                <Typography align="left">
+                <h5>Are You Ready For REAL Income?</h5>
+                <p>
                     Don't let another day go by without taking action. It's time to unlock your true potential with The Non-Fiction Income Method. Start your journey towards financial freedom today and turn your entrepreneurial dreams into reality. Join us now and let's make your success story a reality.
-                </Typography><br />
+                </p><br />
                 <button className='cta-btn' onClick={() => navigate("/product")}>Learn The Non-Fiction Income Method</button>
             </div>
             <Newsletter />
--- client/src/pages/ProductPage.js
+++ client/src/pages/ProductPage.js
@@ -8,11 +8,10 @@ import mastercard from "../media/mastercard.png";
 import americanexpress from "../media/americanexpress.png";
 import paypal from "../media/paypal.png";
 import Newsletter from '../components/NewsLetter';
-import TopBanner from '../components/TopBanner';
 import { loadStripe } from "@stripe/stripe-js";
 import { AddCustomerSessionID } from '../store/actions/userActions';
 import { connect } from "react-redux"
-import { Typography } from '@material-ui/core';
+
 
 const ProductPage = (props) => {
 
@@ -60,7 +59,7 @@ const ProductPage = (props) => {
                 <div className='product-image' style={{ backgroundImage: `url(${book})` }} alt="book image" />
                 <div className='info'>
                     <h6>MoneyStrikers</h6>
-                    <Typography variant="h1">The Non-Fiction Income Method</Typography>
+                    <h1>The Non-Fiction Income Method</h1>
 
 
                     <h4 className='package'>The eBook + Online Tools + Premium Membership</h4>
@@ -84,36 +83,36 @@ const ProductPage = (props) => {
 
             </div><div className='copy'>
                 <iframe width="610px" height="377px" src="https://www.youtube.com/embed/MlCkEl0DkDY?si=Bss5YsxeEu75wpwL" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-                <Typography paragraph>
+                <p>
                     Are you tired of waiting for your online business dreams to come true? With The Non-Fiction Income Method, you can start making money online right away without the need for years of study or training in entrepreneurship.
-                </Typography>  <br /> <br />
-                <Typography variant="h5">Quick and Tangible Results That Propell You To Financial Freedom</Typography>
-                <Typography paragraph>Imagine building your entire business blueprint in just one day. Our comprehensive guide provides you with the exact steps and strategies you need to kickstart your online business journey. No more wasting time on trial and error – get quick and tangible results that will boost your motivation and confidence from day one.</Typography>
+                </p>  <br /> <br />
+                <h5>Quick and Tangible Results That Propell You To Financial Freedom</h5>
+                <p>Imagine building your entire business blueprint in just one day. Our comprehensive guide provides you with the exact steps and strategies you need to kickstart your online business journey. No more wasting time on trial and error – get quick and tangible results that will boost your motivation and confidence from day one.</p>
                 <br /> <br />
-                <Typography variant="h5">The Archetypes of Psychographics For Laser-Focused Marketing</Typography>
-                <Typography paragraph>What sets The Non-Fiction Income Method apart is our unique psychographic approach to market research. Unlock valuable insights into your customers' psychology and gain a competitive advantage in your industry. Say goodbye to guesswork, and hello to targeted marketing strategies that actually work.</Typography>
+                <h5>The Archetypes of Psychographics For Laser-Focused Marketing</h5>
+                <p >What sets The Non-Fiction Income Method apart is our unique psychographic approach to market research. Unlock valuable insights into your customers' psychology and gain a competitive advantage in your industry. Say goodbye to guesswork, and hello to targeted marketing strategies that actually work.</p>
                 <br /> <br />
-                <Typography variant="h5">Leveraging Artificial Intelligence For Maximum Results In Minimum Time</Typography>
-                <Typography paragraph> We also believe in leveraging the power of artificial intelligence to open new doors of opportunity for aspiring entrepreneurs. Discover how to harness the potential of AI to propel your online business to the next level, all while staying ahead of the curve.</Typography>
+                <h5>Leveraging Artificial Intelligence For Maximum Results In Minimum Time</h5>
+                <p > We also believe in leveraging the power of artificial intelligence to open new doors of opportunity for aspiring entrepreneurs. Discover how to harness the potential of AI to propel your online business to the next level, all while staying ahead of the curve.</p>
                 <br /> <br />
-                <Typography variant="h5">Enjoy The Journey By Exercising Your Creativity and Resourcefulness</Typography>
-                <Typography paragraph> The best part? You don't need a massive budget to succeed. With The Non-Fiction Income Method, you can start a 6-figure online company for less than $100. Learn how to leverage your creativity and resourcefulness to maximize your returns and minimize your investment.</Typography>
+                <h5>Enjoy The Journey By Exercising Your Creativity and Resourcefulness</h5>
+                < p> The best part? You don't need a massive budget to succeed. With The Non-Fiction Income Method, you can start a 6-figure online company for less than $100. Learn how to leverage your creativity and resourcefulness to maximize your returns and minimize your investment.</p>
                 <br /> <br />
-                <Typography variant="h5">Short, Simple, and Concise, The eBook Offers You All You Need To Know</Typography>
-                <Typography paragraph> We understand that complex business concepts can be overwhelming, which is why we've made our guide easy to understand with clear and concise chapters. Plus, we provide you with market research templates that will shortcut your way to success.</Typography>
+                <h5>Short, Simple, and Concise, The eBook Offers You All You Need To Know</h5>
+                < p> We understand that complex business concepts can be overwhelming, which is why we've made our guide easy to understand with clear and concise chapters. Plus, we provide you with market research templates that will shortcut your way to success.</p>
                 <br /> <br />
-                <Typography variant="h5">Use Our Business-Builder Online Tool</Typography>
-                <Typography paragraph> But that's not all! When you purchase The Non-Fiction Income Method, you'll also gain access to our online Business Builder tool. Plan, organize, and streamline your business activities for maximum efficiency and productivity.</Typography>
+                <h5>Use Our Business-Builder Online Tool</h5>
+                < p> But that's not all! When you purchase The Non-Fiction Income Method, you'll also gain access to our online Business Builder tool. Plan, organize, and streamline your business activities for maximum efficiency and productivity.</p>
                 <br /> <br />
-                <Typography variant="h5">We Continue To Provide Services</Typography>
-                <Typography paragraph> And as a special bonus, you'll receive a premium membership to all of our current and future services for an entire year. Stay updated with the latest industry trends, tools, and strategies to ensure your continued success.</Typography>
+                <h5>We Continue To Provide Services</h5>
+                <p > And as a special bonus, you'll receive a premium membership to all of our current and future services for an entire year. Stay updated with the latest industry trends, tools, and strategies to ensure your continued success.</p>
                 <br /> <br />
-                <Typography variant="h5">Take Advantage of Our Launching Price</Typography>
-                <Typography paragraph>   Get your hands on The Non-Fiction Income Method today for only $19.99. That's right – the blueprint, tools, and premium membership, all for one affordable price.</Typography>
+                <h5>Take Advantage of Our Launching Price</h5>
+                < p>   Get your hands on The Non-Fiction Income Method today for only $19.99. That's right – the blueprint, tools, and premium membership, all for one affordable price.</p>
                 <br /> <br />
-                <Typography paragraph>  Don't wait any longer – take action now! Upon checkout, you'll receive immediate delivery of your products. Your journey to online success starts here.</Typography>
+                <p >  Don't wait any longer – take action now! Upon checkout, you'll receive immediate delivery of your products. Your journey to online success starts here.</p>
                 <br /> <br />
-                <Typography variant="h4"> Invest in your future with<br /> The Non-Fiction Income Method.<br /> Get started today!</Typography><br /> <br />
+                <h4> Invest in your future with<br /> The Non-Fiction Income Method.<br /> Get started today!</h4><br /> <br />
                 <br />
 
                 <br /><br /> <br />
