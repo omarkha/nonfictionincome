902d9927776cac7b85cd444029aae2b223beaa0d
Copyres <copyresearcher>
2024-01-07 21:02:58 -0500

    topbanner

--- client/src/components/TopBanner.js
+++ client/src/components/TopBanner.js
@@ -1,9 +1,23 @@
 import React from 'react'
 import "../styles/components.css"
-const TopBanner = () => {
+import { connect } from "react-redux"
+const TopBanner = (props) => {
     return (
-        <div className='topbanner'>Use Promocode "SAVE15" for 15% off 🔥</div>
+        <div style={{ display: props.userState.user_id ? "none" : "flex" }} className='topbanner'>Use Promocode "SAVE15" for 15% off 🔥</div>
     )
 }
 
-export default TopBanner;
+const mapStateToProps = (state) => {
+    return {
+        userState: state.user
+    }
+}
+
+const mapActionsToProps = (dispatch) => {
+    return {
+
+    }
+}
+
+
+export default connect(mapStateToProps, mapActionsToProps)(TopBanner);
